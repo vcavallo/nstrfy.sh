72dffca387d991993f6d45209fa1c03fa966fa77
Vinney Cavallo <hi@vinneycavallo.com>
2026-04-13 17:20:31 -0400

    Fix colors in script

--- nstrfy.sh
+++ nstrfy.sh
@@ -11,11 +11,11 @@ DEFAULT_RELAYS="wss://relay.damus.io,wss://nos.lol,wss://relay.nostr.band"
 DEFAULT_EXPIRATION=3600  # 1 hour
 
 # Colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+BLUE=$'\033[0;34m'
+NC=$'\033[0m' # No Color
 
 # Helper functions
 info() {
